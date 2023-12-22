7c0fb61d4df2f7f20ca9d94a3ade1fedcca12576
George David Torres <george@georges-mbp.lan>
2023-12-22 11:44:18 -0600

    echo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI(
@@ -14,14 +15,15 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static",html = True), name="static")
 templates = Jinja2Templates(directory="static")
 
-
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request":request})
 
-@app.get("/ping")
-async def pong():
-    return {"message":"pong"}
+@app.post("/morph")
+async def pong(
+    input_str: str = Form(...)
+):
+    return HTMLResponse(content=f"<p> {input_str}</p>", status_code=200)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
